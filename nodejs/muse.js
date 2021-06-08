38141cf5f2a84fdacd132e6e89670c54baf0eea0
HyunG1u <shgusrb0402@naver.com>
2021-06-08 13:28:32 +0900

    nodejs

---
+++ nodejs/muse.js
@@ -0,0 +1,11 @@
+// module use
+// var m = {
+//     v:'v',
+//     f:function(){
+//         console.log(this.v);
+//     }
+// }
+
+var m = require('./mpart.js');
+
+m.f();
---
+++ password.js
@@ -0,0 +1,4 @@
+module.exports={
+    id:'hyungyu',
+    password:'11111'
+}
